8051092bb14a3830a7151316bf3e9ab25fc25d89
Student <nigelmraydavey@gmail.com>
2025-11-14 10:14:18 -0800

    junit

--- src/test/java/com/SushiAPI/SushiAPI/models/itemTest.java
+++ src/test/java/com/SushiAPI/SushiAPI/models/itemTest.java
@@ -1,6 +1,10 @@
 package com.SushiAPI.SushiAPI.models;
 
+import com.SushiAPI.SushiAPI.models.Sushi.Nigiri;
+import com.SushiAPI.SushiAPI.models.Sushi.Roll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +12,23 @@ class itemTest {
 
     @Test
     void oop() {
+        // act
+        Roll roll = new Roll("aw", 2, 0, new ArrayList<>(), new ArrayList<>());
+        Nigiri nigiri = new Nigiri("test", 2, 0, new ArrayList<>(), new ArrayList<>());
 
+        ArrayList<MenuItem> data = new ArrayList<>();
+        data.add(roll);
+        data.add(nigiri);
+        // action
+        for(MenuItem item : data) {
+            if (item instanceof  Roll) {
+                System.out.println(item.getName() + " is a roll");
+                // assert
+                assertEquals("roll", item.getCategory().toLowerCase());
+            } else if(item instanceof Nigiri) {
+                System.out.println(item.getName() + " is Nigiri");
+                assertEquals("nigiri", item.getCategory().toLowerCase());
+            }
+        }
     }
 }
